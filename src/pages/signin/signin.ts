f145af399fda8f859e2d012c9c5ce6107a94908e
Matt Wilber <mwilber@gmail.com>
2018-02-02 21:50:16 -0500

    Add signin page

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,12 +1,10 @@
+import { SignupPage } from './../signup/signup';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SigninPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 @IonicPage()
 @Component({
@@ -15,11 +13,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SigninPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  signupPage:any = SignupPage;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private authService: AuthService, private loadingCtrl: LoadingController, private alertCtrl: AlertController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SigninPage');
+  onSignin(form: NgForm){
+    const loading = this.loadingCtrl.create({
+      content: 'Signing in...'
+    });
+    loading.present();
+    this.authService.SignIn(form.value.email, form.value.password)
+      .then( data => {
+        loading.dismiss();
+      })
+      .catch( error => {
+        loading.dismiss();
+        const alert = this.alertCtrl.create({
+          title: 'Failed!',
+          message: error.message,
+          buttons: ['Ok']
+        });
+        alert.present();
+      })
   }
 
 }
