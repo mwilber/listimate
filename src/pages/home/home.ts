b40bc17b0f04b663b0ec1604d5ea57346137a596
Matt Wilber <mwilber@gmail.com>
2018-02-10 13:22:40 -0500

    Clean up home page and list srv

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,10 @@
-import { Observable } from 'rxjs/Observable';
-import { OptionsPage } from './options';
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OnInit, Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 import { ListService } from '../../services/list.service';
-import { ShopList } from '../../models/shop-list.model';
 import { ListPage } from '../list/list';
-import { Storage } from '@ionic/storage';
-import { PopoverController } from 'ionic-angular/components/popover/popover-controller';
-import { AuthService } from '../../services/auth.service';
-import { AlertController } from 'ionic-angular/components/alert/alert-controller';
-import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
-import 'rxjs/Rx';
+
 
 @Component({
   selector: 'page-home',
@@ -26,29 +18,11 @@ export class HomePage implements OnInit {
 
   constructor(
     public navCtrl: NavController,
-    private listSrv: ListService,
-    private storage: Storage,
-    private popoverCtrl: PopoverController,
-    private authService: AuthService,
-    private alertCtrl: AlertController,
-    private loadingCtrl: LoadingController
-  ) {
-  }
+    private listSrv: ListService
+  ) {}
 
   ngOnInit(){
-
-    // //Pull saved lists from local storage
-    // this.storage.get('lists').then((val) => {
-    //   if( val ){
-    //     JSON.parse(val).forEach((list, idx)=>{
-    //       this.listSrv.UpdateList(JSON.stringify(list), idx)
-    //     });
-    //     this.RefreshLists();
-    //   }
-    // });
-
     this.InitAddListForm();
-
   }
 
   ionViewDidEnter(){
@@ -67,37 +41,9 @@ export class HomePage implements OnInit {
     this.addListForm.reset();
   }
 
-
-
   onRemoveFromLists(list: any){
-    this.listSrv.RemoveListFromLists(list);
+    this.listSrv.RemoveList(list);
   }
 
-  onShowOptions(event: MouseEvent){
-    const loading = this.loadingCtrl.create({
-      content: 'Please wait...'
-    });
-    const popover = this.popoverCtrl.create(OptionsPage);
-    popover.present({ev: event});
-    popover.onDidDismiss(
-      data => {
-        if(!data){
-          return;
-        }
-        if(data.action == 'connect'){
-          this.lists = this.listSrv.FirebaseConnect();
-        }
-      }
-    )
-  }
-
-  private HandleError(errorMsg: string){
-    const alert = this.alertCtrl.create({
-      title: 'An error occurred!',
-      message: errorMsg,
-      buttons: ['Ok']
-    });
-    alert.present();
-  }
 
 }
--- src/services/list.service.ts
+++ src/services/list.service.ts
@@ -1,51 +1,20 @@
 import { ShopList } from './../models/shop-list.model';
-import { OnInit, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Item } from './../models/item.model';
-import { Subject } from 'rxjs/Subject';
-import { Http } from "@angular/http";
-import 'rxjs/Rx';
+import { Subject, Observable } from 'rxjs';
 import { AuthService } from './auth.service';
-import { Response } from '@angular/http/src/static_response';
-import { Observable } from 'rxjs/Observable';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 @Injectable()
-export class ListService implements OnInit{
+export class ListService{
   private listsDb: AngularFireList<ShopList>;
   public listSave: Subject<boolean>;
   public listChanged: Subject<ShopList[]>;
-
   lists: Observable<any[]>;
-
   listMeta: {key:string, name:string}[];
 
-  constructor(private http: Http, private authService: AuthService, private afDB: AngularFireDatabase) {
-
-    //this.listSave = new Subject<boolean>();
-
-    //this.listChanged = new Subject<ShopList[]>();
-
-
-
-    //this.lists = [new ShopList('Grocery',0,0),new ShopList('Costco',0,0)];
-    //this.lists = [];
-    // this.AddList('Grocery');
-    // this.AddList('Costco');
-    // this.lists[0].AddItems([
-    //   new Item('Bread', 1, 0),
-    //   new Item('Eggs',4,0),
-    //   new Item('Milk',2,0),
-    //   new Item('Beef',1,0)
-    // ]);
-
-    //console.log(this.lists);
-
-
+  constructor(private authService: AuthService, private afDB: AngularFireDatabase) {
     this.listMeta = [];
-  }
-
-  ngOnInit(){
-
   }
 
   FirebaseConnect(){
@@ -71,22 +40,10 @@ export class ListService implements OnInit{
 
   AddList(name: string){
     this.listsDb.push( new ShopList(name, 0, 0) );
-    // this.lists[this.lists.length-1].listChanged.subscribe((data)=>{
-    //   console.log("list change detected");
-    //   this.SaveLists();
-    // });
-    console.log(this.lists);
-    //this.SaveLists();
-    //return this.lists.length-1;
-    //return -1;
-    //this.lists.map();
   }
 
   AddItemToList(listIdx: string, item: Item){
     const userId = this.authService.GetActiveUser().uid;
-    //this.lists[listIdx].AddItem(item.name, item.qty);
-    //this.lists[listIdx].items.push(new Item(item.name, item.qty, 0));
-    //this.SaveLists();
     this.afDB.list<Item>(userId+'/lists/'+listIdx+'/items').push(item);
   }
 
@@ -99,78 +56,17 @@ export class ListService implements OnInit{
     return this.listMeta[listIdx];
   }
 
-  // RemoveList(index: number){
-  //   //this.lists.splice(index, 1);
-  //   this.SaveLists();
-  // }
-
-  RemoveListFromLists(list: any){
+  RemoveList(list: any){
     this.listsDb.remove(list);
-    // const position = this.lists.findIndex((listEl: ShopList)=>{
-    //   return listEl.name === list.name;
-    // });
-    // this.lists.splice(position, 1);
-    // this.SaveLists();
   }
 
-  // UpdateList(listJSON:string, listIdx:number){
-
-  //   this.lists[listIdx] = new ShopList('',0,0);
-  //   this.LoadJson(listJSON,this.lists[listIdx]);
-  //   this.SaveLists();
-  // }
-
-  LoadJson(jsonData: string, shopList: ShopList){
-    let rawData = JSON.parse(jsonData);
-
-    shopList.name = rawData.name;
-    shopList.total = rawData.total;
-    shopList.estimate = rawData.estimate;
-
-    rawData.items.forEach((item)=>{
-      shopList.items.push(new Item(item.name, item.qty, item.price, item.complete));
-    });
+  UpdateList(listId: string, list: ShopList){
+    this.listsDb.update(listId, list);
   }
 
-  // SaveLists(){
-  //   this.listSave.next(true);
-  //   //console.log('all lists saved', this.lists);
-  //   //this.storage.set('lists',JSON.stringify(this.lists));
-  // }
-
-
-
-  RemoteStore(token: string){
-
+  UpdateListTotals(listId: string, newTotal: number, newEstimate: number){
     const userId = this.authService.GetActiveUser().uid;
-    this.afDB.list(userId).set('lists',this.lists);
-    // return this.http
-    //   .put('https://listimate.firebaseio.com/'+userId+'/lists.json?auth='+token, this.lists)
-    //   .map((response: Response)=>{
-    //     return response.json();
-    //   });
-  }
-
-  RemoteFetch(token: string){
-
-    const userId = this.authService.GetActiveUser().uid;
-    return this.http
-      .get('https://listimate.firebaseio.com/'+userId+'/lists.json?auth='+token)
-      .map((response: Response)=>{
-        const ingredients: ShopList[] = response.json() ? response.json() : [];
-        for(let ingredient of ingredients){
-          if(!ingredient.hasOwnProperty('name')){
-            ingredient.name = "unnamed";
-          }
-        }
-        return ingredients;
-      })
-      .do((data)=>{
-        //this.ingredients = data;
-      });
-
-
-
+    this.afDB.list(userId+'/lists').update(listId, {total: newTotal, estimate: newEstimate});
   }
 
 }
