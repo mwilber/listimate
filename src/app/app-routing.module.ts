15cb6b855ea9b0a7f515a6c0cdf9e6f59e3679f3
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-01-10 14:36:59 -0500

    generate placeholder services and pages from legacy project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'list', loadChildren: './list/list.module#ListPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
 ];
 
 @NgModule({
