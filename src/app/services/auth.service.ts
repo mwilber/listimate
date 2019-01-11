364afc067256080117c4574d03d16352da94c50c
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-01-10 23:35:18 -0500

    Install firebase. Port auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  public uid = null;
+
+  SignUp(email: string, password: string){
+    return firebase.auth().createUserWithEmailAndPassword(email, password);
+  }
+
+  SignIn(email: string, password: string){
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  }
+
+  LogOut(){
+    firebase.auth().signOut();
+  }
+
+  GetActiveUser(){
+      return firebase.auth().currentUser;
+
+  }
 }
